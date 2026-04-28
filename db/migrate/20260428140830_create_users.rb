64aff11daa5ca78a211330012090576044ce214f
Bam <ccdda450@gmail.com>
2026-04-28 21:16:00 +0700

    fix: add not null to preventing the duplication

--- db/migrate/20260428140830_create_users.rb
+++ db/migrate/20260428140830_create_users.rb
@@ -10,7 +10,7 @@ class CreateUsers < ActiveRecord::Migration[8.1]
 
       t.timestamps
     end
-    add_index :users, :keycloak_uid
-    add_index :users, :email
+    add_index :users, :keycloak_uid, unique: true
+    add_index :users, :email, unique: true
   end
 end
